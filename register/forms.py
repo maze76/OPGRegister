35ecfa102d19cd34ee81c40bf8c921ae6dea7d3f
maze76 <maze76.new@gmail.com>
2024-03-09 14:25:51 +0100

    locale and templates

--- register/forms.py
+++ register/forms.py
@@ -2,16 +2,48 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Company
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
+from django.core.exceptions import ValidationError
+from django.core.validators import EMPTY_VALUES
+
+
+# def validate_email_contains_at(value):
+#     if '@' not in value:
+#         raise ValidationError(_('Email must contain @ symbol.'))
+    
+
+def validate_not_empty(value):
+    if value in EMPTY_VALUES:
+        raise forms.ValidationError(_("This field is required."))
+
+def validate_numeric(value):
+    if not value.isdigit():
+        raise ValidationError(_('Phone number must contain only numeric characters.'))
+
 
 class UserForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
+    email = forms.EmailField(max_length=100, help_text=_('Required. Enter a valid email address.'))
 
     class Meta:
         model = User
         fields = ('username','email', 'password1', 'password2')
+        labels = {
+            'username': _('username'),
+            'email': _('email'),
+            'password1': _('password1'),
+            'password2': _('password2'),
+        }
+        error_messages = {
+            'username': {'required': _('Please fill out this field.')},
+            'email': {'required': _('Please fill out this field.')},
+            'password1': {'required': _('Please fill out this field.')},
+            'password2': {'required': _('Please fill out this field.')},
+        }
+
 
 class CompanyForm(forms.ModelForm):
+    # first_name = forms.CharField(error_messages={'required': _('Please fill out this field.')})
+
     class Meta:
         model = Company
         fields = ('first_name', 'last_name', 'company_name', 'address', 'phone')
@@ -21,6 +53,13 @@ class CompanyForm(forms.ModelForm):
             'company_name': _('company_name'),
             'address': _('address'),
             'phone': _('phone'),
+        }
+        error_messages = {
+            'first_name': {'required': _('Please fill out this field.')},
+            'last_name': {'required': _('Please fill out this field.')},
+            'company_name': {'required': _('Please fill out this field.')},
+            'address': {'required': _('Please fill out this field.')},
+            'phone': {'required': _('Please fill out this field.')},
         }
 
 class LoginForm(forms.Form):
--- register/models.py
+++ register/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
+
+
+# phone_regex = RegexValidator(regex=r'^\+?1?\d{7,17}$', 
+#             message="Broj može biti samo u formi +777777777777 max 17 brojeva")
+phone_regex = RegexValidator(regex=r'^\+?1?\d{7,17}$')
 
 
 class ProductCategory(models.Model):
@@ -31,7 +38,7 @@ class Company(models.Model):
     last_name = models.CharField(max_length=60)
     company_name = models.CharField(max_length=60)
     address = models.CharField(max_length=60)
-    phone = models.CharField(max_length=15)
+    phone = models.CharField(max_length=15, validators=[phone_regex])
     products = models.ManyToManyField(Product, blank=True)
 
     def __str__(self):
